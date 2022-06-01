6a0d401e82c841edc34237a497057b0018c3d093
Jose_ <joseamsilva100@gmail.com>
2022-06-01 15:10:46 +0100

    exercise 1 a) - point d (done)

--- src/main/java/Presentation/App.java
+++ src/main/java/Presentation/App.java
@@ -6,8 +6,12 @@ public class App {
     @SuppressWarnings("unchecked")
     public static void main( String[] args ) throws Exception {
         D test_d = new D();
-        test_d.addPrivateClient(32564823, 658952648, "Luis Silva", "962568965", "Rua Andre Almeida", null);
-        //test_d.processUnprocessedRegistration();
+//        test_d.removePrivateClient(75485454); //Functional
+//        test_d.addPrivateClient(11112222, 200000011, "Teste to Test", "999111222", "Rua dos Testes", 765865645);
+//        test_d.addPrivateClient(11112223, 200100011, "Teste to Test", "999111222", "Rua dos Testes", 0);
+//        test_d.updatePrivateClient(10000024,200110011, "Edgar Calabroso", "999111222", "Rua dos Testes",200000011,765865645);
+//        test_d.removePrivateClient(11112223);
+
     }
 }
 
--- src/main/java/exc1/a/D.java
+++ src/main/java/exc1/a/D.java
@@ -8,15 +8,21 @@ public class D {
             EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
-
-            Query q = em.createNativeQuery("call add_to_clienteparticular(?1, ?2, ?3, ?4, ?5, ?6)");
+            Query q;
+            if (cpRef == 0){
+                q = em.createNativeQuery("call add_to_clienteParticular(?1, ?2, ?3, ?4, ?5)");
+            }
+            else {
+                q = em.createNativeQuery("call add_to_clienteParticular(?1, ?2, ?3, ?4, ?5, ?6)");
+                q.setParameter(6, cpRef);
+            }
+            //Query q = em.createNativeQuery("call add_to_clienteParticular(?1, ?2, ?3, ?4, ?5, ?6)");
 
             q.setParameter(1, cc);
             q.setParameter(2, nif);
             q.setParameter(3, name);
             q.setParameter(4, telephone);
             q.setParameter(5, address);
-            q.setParameter(6, cpRef);
 
             q.executeUpdate();
             em.getTransaction().commit();
@@ -30,21 +36,18 @@ public class D {
         }
     }
 
-    public int removePrivateClient(Integer cc) throws Exception {
+    public void removePrivateClient(Integer cc) throws Exception {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("t41dg8");
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
 
-            Query q = em.createNativeQuery("select remove_from_clienteParticular(?1)");
+            Query q = em.createNativeQuery("call remove_from_clienteParticular(?1)");
 
             q.setParameter(1, cc);
 
-            int a = q.executeUpdate();
-
+            q.executeUpdate();
             em.getTransaction().commit();
-
-            return a;
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -62,7 +65,7 @@ public class D {
         try {
             em.getTransaction().begin();
 
-            Query q = em.createNativeQuery("select update_from_clienteParticular(?1, ?2, ?3, ?4, ?5, ?6, ?7)");
+            Query q = em.createNativeQuery("call update_from_clienteParticular(?1, ?2, ?3, ?4, ?5, ?6, ?7)");
 
             q.setParameter(1, cc);
             q.setParameter(2, nif);
