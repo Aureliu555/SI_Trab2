dfada2e5cd6b4baa21ab4db0df84b2a51d1b0818
Aureliu555 <A48285@alunos.isel.pt>
2022-06-11 15:48:47 +0100

    presentation package organized

--- src/main/java/Presentation/App.java
+++ src/main/java/Presentation/App.java
@@ -8,7 +8,7 @@ public class App {
     @SuppressWarnings("unchecked")
     public static void main(String[] args) throws Exception {
 //        NoStoredProcedure test_b = new NoStoredProcedure();
-//        test_b.createAndAssociateVehicle("20-PI-40", 100000000, "Afonso Henriques", "912115678", 0, 2, 2.88, null, null);
+//        test_b.createAndAssociateVehicle("20-PI-40", 100000000, "Afonso Henriques", "912115678", 0, 2, 2.88, null, null)..;
         D test_d = new D();
         test_d.addPrivateClient(44112233, 103103103,"Gustavo Lima","922887700", "Rua Castanheira do Sul N11", 659486259);
     }
