17b41ec8bff03c233794dd2bfcc55828eae00302
Jose_ <joseamsilva100@gmail.com>
2022-06-12 14:56:01 +0100

    Exercise 2a and 2b almost functional.

--- src/main/java/exc2/b/TestOptimisticLocking.java
+++ src/main/java/exc2/b/TestOptimisticLocking.java
@@ -1,13 +1,8 @@
-package Presentation;
+package exc2.b;
 
-import exc1.a.*;
-import exc1.b.*;
-import exc1.c.*;
-import exc2.a.*;
 import exc2.a.F;
 
-public class App {
-    @SuppressWarnings("unchecked")
+public class TestOptimisticLocking {
     public static void main(String[] args) throws Exception {
         F testF = new F();
         testF.processUnprocessedRegisters();
