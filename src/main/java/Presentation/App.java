6008c0fae423b7926be612d205d41a247d76c175
Aureliu555 <A48285@alunos.isel.pt>
2022-06-13 19:28:46 +0100

    app test function (comment added)

--- src/main/java/Presentation/App.java
+++ src/main/java/Presentation/App.java
@@ -1,15 +1,21 @@
 package Presentation;
 
-import exc1.a.*;
 import exc1.b.*;
-import exc1.c.*;
-import exc2.a.*;
-import exc2.a.F;
 
 public class App {
     @SuppressWarnings("unchecked")
+
     public static void main(String[] args) throws Exception {
-        NoStoredProcedure test_h = new NoStoredProcedure();
-        test_h.createAndAssociateVehicle("10-PG-20",689425389,"Pedro Miguel", "912345678",0,2, null, null, null);
+        try {
+
+            NoStoredProcedure test_h = new NoStoredProcedure();
+
+            // tests the function from exercise 2.g) from phase 1 made with the use of no stored procedure
+            test_h.createAndAssociateVehicle("10-PG-20",689425389,"Pedro Miguel", "912345678",0,2, null, null, null);
+
+        } catch (Exception e) {
+            throw e;
+        }
+
     }
 }
